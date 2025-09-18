5f2ae1f0ba3d97f0d28a7ca5bb6f3ae772815e84
Jonatas <anjos92489@gmail.com>
2025-09-18 17:48:07 -0300

    adicionando novo css at 02

--- atividades_eliel_02/Atividade_Texto/index.php
+++ atividades_eliel_02/Atividade_Texto/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atividade Eliel</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style_at_02_txt.css">
 </head>
 <body>
     <header>
